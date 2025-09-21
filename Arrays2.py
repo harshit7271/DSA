a11aa04606f85a13c904584c2f763ca0b6e36e65
Harshit Singh <harshitsingh05893312@gmail.com>
2025-09-21 20:26:13 +0530

    Update Arrays2.py

--- Arrays2.py
+++ Arrays2.py
@@ -69,7 +69,22 @@ k = 3
 solution = Solution()
 print(solution.maxSubArrayLen(nums, k))
 
-
+# PLUS ONE
+class Solution:
+    def plusOne(self, digits):
+        n = len(digits)
+        for i in range(n - 1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        
+        return [1] + digits
+    
+# Example usage:
+digits = [1, 2, 3]
+solution = Solution()   
+print(solution.plusOne(digits))
 
 
 
