37a55ac869e3d20c32b6554af0da5e4a949e167c
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-28 08:54:44 +0530

    Updated

--- List/removeDuplicates.py
+++ List/removeDuplicates.py
@@ -2,5 +2,14 @@ numbers = [1, 2, 2, 3, 4, 4, 5]
 
 # first approach
 # this remove the duplicates but will not maintain the oorder of the elements
-set_num = set(numbers)
+"""
+set_num = list(set(numbers))
 print(set_num)
+"""""
+
+# second approach
+unique_elements = []
+for i in numbers:
+    if i not in unique_elements:
+        unique_elements.append(i)
+print(unique_elements)
