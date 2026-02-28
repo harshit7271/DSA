58dfe7de1de51744ffea1a8ffd890f4e39f70471
Harshit Singh <harshitsingh05893312@gmail.com>
2026-02-28 09:19:22 +0530

    Updated

--- List/reverseAList.py
+++ List/reverseAList.py
@@ -1,1 +1,13 @@
 data = [4, 6, 2, 3, 1, 7, 5]
+
+# slicing method
+"""
+reversed_data = data[::-1]
+print(reversed_data)
+"""
+n = len(data)
+reversed_data = []
+
+for i in range(n-1, -1, -1):
+    reversed_data.append(data[i])
+print(f'{reversed_data} is the reversed list')
