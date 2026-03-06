67724230e2f06a12f39b6ad593c8bc5a45833be2
Harshit Singh <harshitsingh05893312@gmail.com>
2026-03-06 19:07:43 +0530

    Updated

--- Preparation/OA/universal.py
+++ Preparation/OA/universal.py
@@ -11,6 +11,32 @@ def reverseNum(num):
 
 print(reverseNum(1234))
 
+
+data = [4, 6, 2, 3, 1, 7, 5]
+
+# slicing method
+"""
+reversed_data = data[::-1]
+print(reversed_data)
+"""
+n = len(data)
+reversed_data = []
+
+for i in range(n-1, -1, -1):
+    reversed_data.append(data[i])
+print(f'{reversed_data} is the reversed list')
+
+
+"""
+n = len(data) - 1
+reversed_data2 = []
+while n >= 0:
+    reversed_data2.append(data[n])
+    n -= 1
+print(reversed_data2)
+
+"""
+
 # reverse a string
 
 
