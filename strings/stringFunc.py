4db611a1aa96c67bdfff44ba90cb9c78e7534713
Harshit Singh <harshitsingh05893312@gmail.com>
2026-03-06 16:23:35 +0530

    Update

--- strings/stringFunc.py
+++ strings/stringFunc.py
@@ -69,3 +69,13 @@ def maxSubArrar2(self, nums):
         if total < 0:
             total = 0
     return maxi
+
+
+# move zeros
+
+def moveZeros(self, nums: List[int]) -> None:
+    j = 0
+    for i in range(len(nums)):
+        if nums[i] != 0:
+            nums[j], nums[i] = nums[i], nums[i]
+            j += 1
