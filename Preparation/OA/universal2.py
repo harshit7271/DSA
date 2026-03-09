11e97f3db3954e3253b0fea242b4c4c6c5c4d130
Harshit Singh <harshitsingh05893312@gmail.com>
2026-03-09 15:06:25 +0530

    updated

--- Preparation/OA/universal2.py
+++ Preparation/OA/universal2.py
@@ -13,3 +13,36 @@ def merge(self, intervals):
         else:
             result[-1][1] = max(result[-1][1], interval[1])
     return result
+
+
+# Sort colors
+"""[2,0,2,1,1,0]"""
+
+
+class Solution(object):
+
+    def sortColors(self, nums):
+        """
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[j] <= nums[i]:
+                    nums[i], nums[j]= nums[j], nums[i]
+        return nums           
+        """
+
+        red = 0
+        white = 0
+        blue = len(nums) - 1
+
+        while white <= blue:
+            curr = nums[white]
+            if curr == 0:
+                nums[white], nums[red] = nums[red], nums[white]
+                red += 1
+                white += 1
+            elif curr == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+        return nums
