be70036ffa1cc543d8df9c989190a6ab58bba618
Harshit Singh <harshitsingh05893312@gmail.com>
2026-03-17 18:46:28 +0530

    Update

--- Preparation/OA/universal2.py
+++ Preparation/OA/universal2.py
@@ -177,3 +177,21 @@ def canContruct(self, ransomNote, magazine):
             return False
         d[char] -= 1
     return True
+
+
+# Isomorphic String
+
+def isIsomorphic(self, s, t):
+    d = {}
+    values = set()
+
+    for i, char in enumerate(s):
+        if char not in d:
+            if t[i] in values:
+                return False
+            d[char] = t[i]
+            values.add(t[i])
+        else:
+            if d[char] != t[i]:
+                return False
+    return True
